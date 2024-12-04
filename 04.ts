d3b8b5fa1faf7075de842fb71f6a81ee97b4138a
Logan Ingalls <lingalls@gmail.com>
2024-12-04 08:43:36 -0500

    Day 4, part 1. I suspect I won't reuse most of my code for part 2

---
+++ 04.ts
@@ -0,0 +1,79 @@
+import { toLinesArray, toIntsArray, fileContents } from './util.ts'
+
+function loadData() {
+  return toLinesArray(fileContents('data/04'))
+}
+
+function part1(d: string[]) {
+  let sum = XmasCountE(d) + XmasCountSE(d);
+  for (i = 0; i <= 2; i++) {
+    d = rotateCCW(d);
+    sum += XmasCountE(d) + XmasCountSE(d);
+  }
+  return sum
+}
+
+function XmasCountE(d: string[]) {
+  let sum = 0;
+  d.forEach((l) => {
+    const c = l.match(/XMAS/g)?.length;
+    if (c !== undefined) sum += c;
+  });
+  return sum;
+}
+
+function XmasCountSE(d: string[]) {
+  const height = d.length;
+  const width = d[0].length;
+  let diags = [];
+  // Diagonals starting from (0,0) to (0,width-4)
+  for (let x = 0; x <= width-4; x++) {
+    let diagLine = "";
+    for (let i = 0; x+i < width && i < height; i++) {
+      diagLine += d[i][x+i];
+    }
+    diags.push(diagLine);
+  }
+  // Diagonals starting from (1,0) to (height-4,0)
+  for (let y = 1; y <= height-4; y++) {
+    let diagLine = "";
+    for (let i = 0; i < width && y+i < height; i++) {
+      diagLine += d[y+i][i];
+    }
+    diags.push(diagLine);
+  }
+  return XmasCountE(diags);
+}
+
+function rotateCCW(d: string[]) {
+  let r = [];
+  d.forEach((row) => {
+    for (let x = 0; x < row.length; x++) {
+      let newy = d.length - x - 1;
+      if (r[newy] === undefined) r[newy] = row[x];
+      else r[newy] += row[x];
+    }
+  });
+  return r
+}
+
+function part2(d: string[]) {
+  return d.length;
+}
+
+let sampleData = toLinesArray(
+`MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX`);
+
+console.log("part1(sampleData) = " + part1(sampleData));
+console.log("part1 = " + part1(loadData()));
+// console.log("part2(sampleData) = " + part2(sampleData));
+// console.log("part2 = " + part2(loadData()));
